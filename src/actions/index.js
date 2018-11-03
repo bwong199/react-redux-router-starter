0154513a530efa3b4e5729a9822a2cdcbaf55fc4
bwong199 <bwong199@my.bcit.ca>
2018-11-02 22:54:49 -0600

    wired component to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ const API_KEY = '?key=Wongbmw1'
 export function fetchPosts(){
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     return {
-        type: FETCH_POSTS
+        type: FETCH_POSTS,
+        payload: request
     }
 }

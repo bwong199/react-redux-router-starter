f034a67a8945f8cba81a6f6a4b06e720ce8902bb
bwong199 <bwong199@my.bcit.ca>
2018-11-02 21:10:16 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello!
+        </div>
+        )
+    }
+}
+
+class Goodbye extends React.Component {
+    render() {
+        return (
+            <div>
+                Goodbye!
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={Hello} />
+                <Route path="/goodbye" component={Goodbye} />
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));

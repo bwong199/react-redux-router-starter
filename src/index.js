e0acbf2673eb14ec83a4a20ec2d38bf8672fb68a
bwong199 <bwong199@my.bcit.ca>
2018-11-02 21:21:42 -0600

    got react-router working

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello!
-        </div>
-        )
-    }
-}
-
-class Goodbye extends React.Component {
-    render() {
-        return (
-            <div>
-                Goodbye!
-            </div>
-        )
-    }
-}
-
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
-                <Route path="/" component={Hello} />
-                <Route path="/goodbye" component={Goodbye} />
+                <Route path="/" component={PostsIndex}/>
             </div>
         </BrowserRouter>
     </Provider>
